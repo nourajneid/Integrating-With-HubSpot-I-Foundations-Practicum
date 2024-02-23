12b9fa24b027ff6f53c2f8b5f6f65924d3866f56
noura jneid <noura.n.jneid@gmail.com>
2024-02-24 01:43:18 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Use only one port variable
+
+app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Route for the homepage
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.hubapi.com/crm/v3/objects/contacts', {
+            headers: {
+                Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`, // Include the API key in the request header
+                'Content-Type': 'application/json'
+            }
+        });
+        const records = res.json(response.data);
+        console.log(records)
+        res.render('homepage', { records: records });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching CRM records');
+    }
+});
+
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
+
+app.post('/update-cobj', async (req, res) => {
+    try {
+        const response = await axios.post('https://api.hubapi.com/crm/v3/objects/contacts', {
+            properties: {
+                // Assuming 'name' is a valid property for your HubSpot account
+                // Otherwise, use 'firstname', 'lastname', 'email', etc.
+                firstname: req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email
+            }
+        }, {
+            headers: {
+                Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`, // Include the API key in the request header
+                'Content-Type': 'application/json'
+            }
+        });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while creating a CRM record');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}).on('error', (err) => {
+    console.error('Failed to start server:', err);
+});
